666371da51450e778ab7cee1b8f0fcf0223c9303
karanja03 <wambuik03@gmail.com>
2023-06-16 12:48:50 +0300

    complete first class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Code your solution in this file!
+const drivers =['Antonia', 'Nuru', 'Amari', 'Mo'];
+ function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2);
+
+ }
+ console.log(drivers);
+ function returnLastTwoDrivers(drivers){
+    return drivers.slice(2,4);
+
+ }
+ console.log(drivers)
+
+ let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+ console.log(selectingDrivers)
+
+ function createFareMultiplier(fareQuintupler){
+     return function(fareMultiplier){
+  fareQuintupler = 5;
+ fareMultiplier = 2;
+    
+
+    return fareQuintupler ** fareMultiplier;
+   }
+ }
+ console.log(createFareMultiplier())
+
+ function fareDoubler(fare){
+    return fare * 2;
+ }
+ console.log(fareDoubler)
+
+ function fareTripler(fare){
+  return fare * 3 ;
+ }
+ console.log(fareTripler)
+
+
+ function selectDifferentDrivers(drivers, arrayOfDrivers){
+return arrayOfDrivers(drivers)
+ }
+ console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers))
+ console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers))
+     
+
+
+    
+ 
+
+
+
+
